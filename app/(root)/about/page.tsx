49f3691dad15c0e8c1b32d41098625c22da44519
Viraj Waim <virajwaim123@gmail.com>
2024-01-11 18:03:04 +0530

    working

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,3 +1,4 @@
+import FeatureCard from '@/components/shared/cards/FeatureCard';
 import SectionHeader from '@/components/shared/header/sectionHeader';
 import Image from 'next/image';
 
@@ -17,13 +18,13 @@ export default function About() {
         </div>
       </section>
 
-      {/* About Section */}
+      {/* About Finosophy Section */}
       <section className="py-20">
         <div className="container grid gap-14 md:grid-cols-2">
           <div className="flex flex-col gap-y-8">
             <SectionHeader
               subheading="About Finosophy"
-              heading="The right environment for growing"
+              heading="Financial Education Made Easy"
             />
             <p className="">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
@@ -39,6 +40,45 @@ export default function About() {
               height={500}
               alt="About image"
               className="w-full rounded-3xl"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* About Letstute Section */}
+      <section className="bg-white py-20">
+        <div className="container flex flex-col gap-y-16">
+          <div className="flex flex-col gap-y-8">
+            <SectionHeader
+              style="items-center"
+              subheading="About Letstute"
+              heading="EdTech Company Based in Mumbai"
+              headingStyle="text-center"
+            />
+            <p className="text-center text-lg">
+              Letstute (Universal Learning Aid Pvt Ltd) is an E-learning company
+              based in Mumbai, India. Letstute today is 9 years old. Our goal
+              from the beginning is to create a meaningful and a positive impact
+              in the education segment by providing quality education content to
+              everyone who wants to explore, visualize and apply their learning
+              to real life.
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-20">
+            <FeatureCard
+              style="bg-white"
+              imgSrc=""
+              heading="Our Vision"
+              desc="Our vision is to make sure that we provide our students with quality education. Education is something that we consider as our top priority.
+              We encourage our students to gain more and more knowledge along with providing them with our excellent education sources. We also believe in giving equal education to all."
+            />
+            <FeatureCard
+              style="bg-white"
+              imgSrc=""
+              heading="Our Goal"
+              desc="Our goal is to fulfill your dreams of turning your passion into a beautiful profession!
+              We do believe in providing not just good education but also to make sure that our students perceive their dreams after completing the courses.
+              Our joy is in seeing our students excel in whatever course that they have applied for."
             />
           </div>
         </div>
